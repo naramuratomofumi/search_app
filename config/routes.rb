9545d925a5860cc8bbe55249d57a1860e79bc328
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-17 15:43:50 +0900

    collectionを用いてルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-  resources :items, only: [:new, :create, :show, :edit, :update]
+  resources :items, only: [:new, :create, :show, :edit, :update] do
+    collection do       # collection /items/searchのようにidを伴わないパスを追加するときに使う。
+      get 'search'
+    end
+  end
 end
